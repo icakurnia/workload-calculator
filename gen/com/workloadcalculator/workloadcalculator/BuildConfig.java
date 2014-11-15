c556d67b2b8e14671dc4e9487f56b111f612d5a5
Ica Kurnia Hildayanti <icakurniahildayanti@gmail.com>
2014-11-15 13:28:08 +0700

    first commit project

---
+++ gen/com/workloadcalculator/workloadcalculator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.workloadcalculator.workloadcalculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
